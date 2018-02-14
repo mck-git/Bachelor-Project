39bf397629de2b01cf1f33c81ffb672b35f82493
s154666 <RGb7rK5w>
2018-02-14 17:05:00 +0100

    Added TODO

--- src/Compile/MainCompiler.java
+++ src/Compile/MainCompiler.java
@@ -30,32 +30,6 @@ public class MainCompiler {
                 break;
         }
 
-//        switch (words[0])
-//        {
-//            case "int":
-//                System.out.println("int declaration!");
-//                Mapper.addToIntMap(
-//                        new IntegerVariable(words[1],Integer.parseInt(words[3]))
-//                );
-//                break;
-//
-//            case "print":
-//                System.out.println("print statement!");
-//                Object[] val = Mapper.findVariable(words[1]);
-//                if ( val[0] == ("int") )
-//                {
-//
-//                    IntegerVariable v =  (IntegerVariable) val[1];
-//
-//                    System.out.println(
-//                            v.getValue()
-//                    );
-//                }
-//                break;
-//
-//        }
-
-
     }
 
 
